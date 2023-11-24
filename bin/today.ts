10d87356c8203fbee2aaf68908b9947a84b386d6
sportshead <me@sportshead.dev>
2023-11-24 12:21:24 +0000

    today.ts: fix padStart for argv

--- bin/today.ts
+++ bin/today.ts
@@ -5,8 +5,8 @@ import { getAOCDay, getAOCYear, projectRoot } from "@utils";
 console.log(
     resolve(
         projectRoot,
-        `years/${process.argv[2] || getAOCYear()}/${
-            process.argv[3] || getAOCDay().toString().padStart(2, "0")
-        }`,
+        `years/${process.argv[2] || getAOCYear()}/${(
+            process.argv[3] || getAOCDay().toString()
+        ).padStart(2, "0")}`,
     ),
 );
